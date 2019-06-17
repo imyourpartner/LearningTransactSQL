722d57e503a28405625054b8a027008c08ff8c03
PC <PC@DESKTOP-PFC4OU0>
2019-06-17 10:20:10 -0700

    commit

---
+++ ConsoleApp/db/AdventureWorks2017/Models/Culture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp.db.AdventureWorks2017.Models
+{
+    public partial class Culture
+    {
+        public Culture()
+        {
+            ProductModelProductDescriptionCulture = new HashSet<ProductModelProductDescriptionCulture>();
+        }
+
+        public string CultureId { get; set; }
+        public string Name { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public virtual ICollection<ProductModelProductDescriptionCulture> ProductModelProductDescriptionCulture { get; set; }
+    }
+}
---
+++ ConsoleApp/db/AdventureWorks2017/Models/ProductCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp.db.AdventureWorks2017.Models
+{
+    public partial class ProductCategory
+    {
+        public ProductCategory()
+        {
+            ProductSubcategory = new HashSet<ProductSubcategory>();
+        }
+
+        public int ProductCategoryId { get; set; }
+        public string Name { get; set; }
+        public Guid Rowguid { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public virtual ICollection<ProductSubcategory> ProductSubcategory { get; set; }
+    }
+}
